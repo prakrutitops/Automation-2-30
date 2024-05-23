4ce518dcd273fa78c0100b6dd26f88f1a4bb3530
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-05-23 15:33:56 +0530

    Add files via upload

---
+++ Module2/23-5/ArrayEx1.java
@@ -0,0 +1,19 @@
+package com.a185;
+
+public class ArrayEx1 {
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		// datatype arr_variable = new datatype[size]
+
+		arr[0] = 10;
+		arr[1] = 10;
+		arr[2] = 10;
+		arr[3] = 10;
+		arr[4] = 10;
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
---
+++ Module2/23-5/ArrayEx2.java
@@ -0,0 +1,13 @@
+package com.a185;
+
+public class ArrayEx2 {
+	public static void main(String[] args) 
+	{
+		int arr[] = {10,20,30,40,50};
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
