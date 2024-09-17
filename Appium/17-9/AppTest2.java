ff087c1b876241853b6eb155428fdbda5fc5c010
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-09-17 13:49:50 +0530

    commit

---
+++ Appium/17-9/AppTest2.java
@@ -0,0 +1,44 @@
+package appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class AppTest2 
+{
+	public static void main(String[] args) throws MalformedURLException,InterruptedException
+	{
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME,MobilePlatform.ANDROID);
+		
+		cap.setCapability("appPackage","com.androidsample.generalstore");
+		
+		cap.setCapability("appActivity","com.androidsample.generalstore.MainActivity");
+		
+		AndroidDriver<MobileElement>driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub/"),cap);
+
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Harsh");
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+	
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
+		
+		Thread.sleep(2000);
+	}
+	
+}
